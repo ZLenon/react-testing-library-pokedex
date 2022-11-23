54556aee2ff59d61e1f4120620f15419fd55a20e
Lenon <lenoninformatica@gmail.com>
2022-11-22 23:26:19 -0300

    🚧Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import NotFound from '../pages/NotFound';
+
+describe('Testando o componente NotFound', () => {
+  it('Testando se a página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+
+    const textNotFound = screen.getByRole('heading', { level: 2, name: 'Page requested not found' });
+    expect(textNotFound).toBeDefined();
+  });
+  it('Testa se a pág mostra a img "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    render(<NotFound />);
+
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imgSrc = screen.getByRole('img', { src: url });
+    expect(imgSrc).toHaveAttribute('src', url);
+  });
+});
